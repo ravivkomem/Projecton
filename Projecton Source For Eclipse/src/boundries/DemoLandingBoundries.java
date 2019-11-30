4b10e1e3d173f5372dbfbd2786fae03bd7eddf5a
leehugi <leehugi93@gmail.com>
2019-11-30 21:45:52 +0200

    a

--- Projecton Source For Eclipse/src/boundries/DemoLandingBoundries.java
+++ Projecton Source For Eclipse/src/boundries/DemoLandingBoundries.java
@@ -1,5 +1,36 @@
 package boundries;
 
+import com.sun.glass.events.MouseEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class DemoLandingBoundries {
+
+    @FXML
+    private TextField changeRequestTextField;
+
+    @FXML
+    private Button viewButton;
+
+    @FXML
+    private TableView<?> changeRequestDetailsTableView;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    void displayChangeRequestDetails(MouseEvent event) {
+    	System.out.println(changeRequestTextField.getText());
+    	changeRequestDetailsTableView.setVisible(true);
+    	updateButton.setVisible(true);
+    }
+
+    @FXML
+    void updateChangeRequestTable(MouseEvent event) {
+
+    }
 
 }
--- Projecton Source For Eclipse/src/boundries/DemoLandingController.java
+++ Projecton Source For Eclipse/src/boundries/DemoLandingController.java
@@ -1,34 +1,6 @@
 package boundries;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
 public class DemoLandingController {
 
-    @FXML
-    private TextField changeRequestTextField;
-
-    @FXML
-    private Button viewButton;
-
-    @FXML
-    private TableView<?> changeRequestDetailsTableView;
-
-    @FXML
-    private Button updateButton;
-
-    @FXML
-    void displayChangeRequestDetails(MouseEvent event) {
-    	System.out.println(changeRequestTextField.getText());
-    	changeRequestDetailsTableView.setVisible(true);
-    	updateButton.setVisible(true);
-    }
-
-    @FXML
-    void updateChangeRequestTable(MouseEvent event) {
-
-    }
 }
