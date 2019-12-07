0070db1cb3947724b0b6e20245580a7b82c016f7
leehugi <leehugi93@gmail.com>
2019-12-07 21:31:54 +0200

    2

---
+++ Projecton Source For Eclipse/src/controllers/PagingController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.io.IOException;
+
+import boundries.ProjectFX;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+
+public class PagingController {
+	
+	public void loadBoundray (String s)
+	{
+		FXMLLoader loader = new FXMLLoader();
+		Pane root;
+		try {
+			root = loader.load(getClass().getResource(s));
+			Scene scene = new Scene(root);			
+			ProjectFX.mainStage.setScene(scene);		
+			ProjectFX.mainStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		BasicController studentFormController = loader.getController();		
+//		studentFormController.loadStudent(Test.students.get(itemIndex));
+		
+		
+	}
+
+}
