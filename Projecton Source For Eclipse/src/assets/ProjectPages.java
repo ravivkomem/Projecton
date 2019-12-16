0d7e93bb993e9091ae635b072ff6f473f5b77175
idokadosh <idokadosh1@gmail.com>
2019-12-16 14:52:26 +0200

    Changed location of files
    Signed-off-by: idokadosh <idokadosh1@gmail.com>

--- Projecton Source For Eclipse/src/assets/ProjectPages.java
+++ Projecton Source For Eclipse/src/assets/ProjectPages.java
@@ -4,8 +4,8 @@ public enum ProjectPages {
 
 	SERVER_START_PAGE("/server/ServerStartGUI.fxml"),
 	CONNECT_TO_SERVER_PAGE("/client/ClientConnectionPage.fxml"),
-	LOGIN_PAGE("/fxml/LoginPage.fxml"),
-	MENU_PAGE("/fxml/MenuPage.fxml"),
+	LOGIN_PAGE("/boundries/LoginPage.fxml"),
+	MENU_PAGE("/boundries/MenuPage.fxml"),
 	DEMO_LANDING_PAGE("/boundries/DemoLandingPage.fxml");
 	
 	private String path;
