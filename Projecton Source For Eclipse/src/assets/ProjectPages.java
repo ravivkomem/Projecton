c2e73155195ae97a509aedc9945e22910e5e0178
leehugi <leehugi93@gmail.com>
2019-12-16 15:58:18 +0200

    finished the ProjectPages.java start workings!!!!!!
    Signed-off-by: leehugi <leehugi93@gmail.com>

--- Projecton Source For Eclipse/src/assets/ProjectPages.java
+++ Projecton Source For Eclipse/src/assets/ProjectPages.java
@@ -6,7 +6,19 @@ public enum ProjectPages {
 	CONNECT_TO_SERVER_PAGE("/client/ClientConnectionPage.fxml"),
 	LOGIN_PAGE("/boundries/LoginPage.fxml"),
 	MENU_PAGE("/boundries/MenuPage.fxml"),
-	DEMO_LANDING_PAGE("/boundries/DemoLandingPage.fxml");
+	DEMO_LANDING_PAGE("/boundries/DemoLandingPage.fxml"),
+	WORK_STATION_PAGE("/boundries/WorkStationPage.fxml"),
+	UPLOAD_REQUEST_PAGE("/boundries/UploadRequestPage.fxml"),
+	TIME_EXTENSION_PAGE("/boundries/TimeExtensionPage.fxml"),
+	TECH_MANAGER_PAGE("/boundries/TechManagerPage.fxml"),
+	SUPERVISOR_PAGE("/boundries/SupervisorPage.fxml"),
+	REQUEST_LIST_PAGE("/boundries/RequestListPage.fxml"),
+	EXTRA_DETAILS_PAGE("/boundries/ExtraDetailsPage.fxml"),
+	EXECUTION_LEADER_PAGE("/boundries/ExecutionLeaderPage.fxml"),
+	COMMITTEE_PAGE("/boundries/CommitteeDecisionPage.fxml"),
+	ANALYZER_PAGE("/boundries/AnalyzerPage.fxml"),
+	ANALISIS_REPORT_PAGE("/boundries/AnalysisReportPage.fxml"),
+	TESTER_PAGE("/boundries/TesterPage.fxml");
 	
 	private String path;
 	
