89ad825c760840b87043f808d83676ec91218056
leehugi <leehugi93@gmail.com>
2019-12-25 18:48:15 +0200

    EmployeePermission.fxml
    Signed-off-by: leehugi <leehugi93@gmail.com>

--- Projecton Source For Eclipse/src/boundries/EmployeePermissionBoundary.java
+++ Projecton Source For Eclipse/src/boundries/EmployeePermissionBoundary.java
@@ -37,36 +37,43 @@ public class EmployeePermissionBoundary implements DataInitializable{
 	
     @FXML
     void setNewEmployeePermission(MouseEvent event) {
-    	if(newPremissionComboBox.getSelectionModel().getSelectedItem().isEmpty()) {
+    	if(newPremissionComboBox.getSelectionModel().isEmpty()) {
     		errorText.setText("Please select permission");
 			return;
 		}
 		switch (newPremissionComboBox.getSelectionModel().getSelectedItem()) {
 		case "INFORMATION_ENGINEER":
-			myController.updateEmployeePermission("INFORMATION_ENGINEER",employeeUser.getUserID());
+			errorText.setVisible(false);
+			myController.updateEmployeePermission("INFORMATION_ENGINEER","Information Engineer",employeeUser.getUserID());
+			employeeUser.setPermission("INFORMATION_ENGINEER");
 			break;
 		case "SUPERVISOR":
+			errorText.setVisible(false);
 			for(User u: users) {
 				if(u.getPermission().equals("SUPERVISOR")) {
 					errorText.setText("There is already user with supervisor permission");
+					errorText.setVisible(true);
 					return;
 				}
 			}
-			myController.updateEmployeePermission("SUPERVISOR",employeeUser.getUserID());
+			employeeUser.setPermission("SUPERVISOR");
+			myController.updateEmployeePermission("SUPERVISOR","Supervisor",employeeUser.getUserID());
 			break;
 		case "COMMITTEE_MEMBER":
+			errorText.setVisible(false);
 			int cnt=0;
 			for(User u: users) {
 				if(u.getPermission().equals("COMMITTEE_MEMBER")) {
 					cnt++;
-					return;
 				}
 			}
 			if(cnt==2) {
 				errorText.setText("There are already 2 users with this permission");
+				errorText.setVisible(true);
 			}
 			else {
-				myController.updateEmployeePermission("COMMITTEE_MEMBER",employeeUser.getUserID());
+				employeeUser.setPermission("COMMITTEE_MEMBER");
+				myController.updateEmployeePermission("COMMITTEE_MEMBER","Committee member",employeeUser.getUserID());
 			}
 			break;
 		default:
@@ -84,7 +91,7 @@ public class EmployeePermissionBoundary implements DataInitializable{
 	@Override
 	public void initData(Object data) {
 		employeeUser = (User)(((ArrayList<ArrayList<Object>>) data).get(0).get(0));
-		users.addAll(((ArrayList<ArrayList<User>>) data).get(0));
+		users.addAll(((ArrayList<ArrayList<User>>) data).get(1));
 		employeeNameText.setText("Permission: "+employeeUser.getFirstName()+" "+employeeUser.getLastName());
 		permossionTextField.setText(employeeUser.getPermission());
 	}
