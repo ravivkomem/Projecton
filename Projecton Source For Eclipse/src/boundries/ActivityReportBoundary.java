2c70b32776213f30b8d2ae59b9b590eb172670b6
leehugi <leehugi93@gmail.com>
2019-12-28 13:57:59 +0200

    add query
    Signed-off-by: leehugi <leehugi93@gmail.com>

--- Projecton Source For Eclipse/src/boundries/ActivityReportBoundary.java
+++ Projecton Source For Eclipse/src/boundries/ActivityReportBoundary.java
@@ -18,48 +18,49 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class ActivityReportBoundary implements DataInitializable{
-	
-	/* *************************************
-	 * ********* FXML Objects **************
-	 * *************************************/
-	
-    @FXML
-    private DatePicker startDatePicker;
-    @FXML
-    private DatePicker endDatePicker;
-    @FXML
-    private Button showDetailsButton;
-    
-    @FXML
-    private AnchorPane activityReportDetailsPane;
-    @FXML
-    private PieChart requestStatusPieChart;
-    @FXML
-    private TextField medianTextField;
-    @FXML
-    private TextField stdTextField;
-    @FXML
-    private TextField distributionTextField;
-    @FXML
-    private TextField handleDaysTextField;
-    
-    /* *************************************
-	 * ******* Private Objects *************
-	 * *************************************/
-    private ActivityReportController myController = new ActivityReportController(this);
-    private ActivityReport activityReport;
-    private ArrayList<ChangeRequest> changeRequestList;
+public class ActivityReportBoundary implements DataInitializable {
 
-    /* *************************************
-	 * ******* FXML Methods *************
-	 * *************************************/
-    
-    @FXML
-    void showActivityReportDetails(MouseEvent event) {
+	/*
+	 * ************************************* ********* FXML Objects **************
+	 *************************************/
+
+	@FXML
+	private DatePicker startDatePicker;
+	@FXML
+	private DatePicker endDatePicker;
+	@FXML
+	private Button showDetailsButton;
+
+	@FXML
+	private AnchorPane activityReportDetailsPane;
+	@FXML
+	private PieChart requestStatusPieChart;
+	@FXML
+	private TextField medianTextField;
+	@FXML
+	private TextField stdTextField;
+	@FXML
+	private TextField distributionTextField;
+	@FXML
+	private TextField handleDaysTextField;
+
+	/*
+	 * ************************************* ******* Private Objects *************
+	 *************************************/
+	private ActivityReportController myController = new ActivityReportController(this);
+	private ActivityReport activityReport;
+	private ArrayList<ChangeRequest> changeRequestList;
+
+	/*
+	 * ************************************* ******* FXML Methods *************
+	 *************************************/
+
+	@FXML
+	void showActivityReportDetails(MouseEvent event) {
 		if (startDatePicker.getValue() == null || endDatePicker.getValue() == null) {
 			Toast.makeText(ProjectFX.mainStage, "Please select start date and end date", 1500, 500, 500);
 		} else {
+			activityReportDetailsPane.setVisible(true);
 			Date startDate = Date.valueOf(startDatePicker.getValue());
 			Date endDate = Date.valueOf(endDatePicker.getValue());
 			Date todayDate = TimeManager.getCurrentDate();
@@ -82,20 +83,61 @@ public class ActivityReportBoundary implements DataInitializable{
 		}
 	}
 
-    /* *************************************
-	 * ******* Public Methods *************
-	 * *************************************/
-    
-    public void createActivityReportList(ArrayList<ChangeRequest> requestList) {
-    	changeRequestList = requestList;
-    	int active = 0, close = 0, suspended = 0, denied = 0, workDatys = 0;
-    	for(int i=0;i<changeRequestList.size();i++) {
-    		if(changeRequestList.get(i).getCurrentStep().equals("ACTIVE")) {
-    			//TODO
-    		}
-    	}
-    }
-    
+	/*
+	 * ************************************* ******* Public Methods *************
+	 *************************************/
+
+	/**
+	 * this method gets ChangeRequest list
+	 * and create an ActivityReport
+	 * @param requestList
+	 */
+	public void createActivityReportList(ArrayList<ChangeRequest> requestList) {
+		changeRequestList = requestList;
+		int active = 0, close = 0, suspended = 0, denied = 0;
+		long workDays = 0;
+		for (int i = 0; i < changeRequestList.size(); i++) {
+			if (changeRequestList.get(i).getStatus().equals("Active")) {
+				active++;
+				long daysBetween = TimeManager.getDaysBetween(changeRequestList.get(i).getStartDate(),
+						TimeManager.getCurrentDate());
+				workDays += daysBetween;
+			} else if (changeRequestList.get(i).getCurrentStep().equals("Denied")) {
+				denied++;
+				long daysBetween = TimeManager.getDaysBetween(changeRequestList.get(i).getStartDate(),
+						changeRequestList.get(i).getEndDate());
+				workDays += daysBetween;
+			} else if (changeRequestList.get(i).getCurrentStep().equals("Suspended")) {
+				suspended++;
+				long daysBetween = TimeManager.getDaysBetween(changeRequestList.get(i).getStartDate(),
+						TimeManager.getCurrentDate());
+				workDays += daysBetween;
+			} else if(changeRequestList.get(i).getCurrentStep().equals("Close")) {
+				close++;
+				long daysBetween = TimeManager.getDaysBetween(changeRequestList.get(i).getStartDate(),
+						changeRequestList.get(i).getEndDate());
+				workDays += daysBetween;
+			}
+		}
+		displayActivityReport(activityReport = new ActivityReport(active, close, suspended,denied, workDays));
+		
+	}
+	
+	/**
+	 * this method display the report in the page
+	 * @param report
+	 */
+	private void displayActivityReport(ActivityReport report) {
+		requestStatusPieChart.getData().add( new PieChart.Data("Active", report.getActiveChageRequest()));
+		requestStatusPieChart.getData().add( new PieChart.Data("Close", report.getCloseChangeRequest()));
+		requestStatusPieChart.getData().add( new PieChart.Data("Suspended", report.getSuspendedChangeRequest()));
+		requestStatusPieChart.getData().add( new PieChart.Data("Denied", report.getDeniedChangeRequest()));
+		handleDaysTextField.setText(""+report.getSpentWorkDays());
+		medianTextField.setText("");
+		stdTextField.setText("");
+		distributionTextField.setText("");
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		activityReportDetailsPane.setVisible(false);
@@ -103,7 +145,7 @@ public class ActivityReportBoundary implements DataInitializable{
 
 	@Override
 	public void initData(Object data) {
-		
+
 	}
 
 }
