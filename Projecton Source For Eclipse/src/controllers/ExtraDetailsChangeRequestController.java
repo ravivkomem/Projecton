28f92385ae069c59442902739ce68cc4fc7b2e50
idokadosh <idokadosh1@gmail.com>
2020-01-07 17:14:52 +0200

    wokred on my pages

--- Projecton Source For Eclipse/src/controllers/ExtraDetailsChangeRequestController.java
+++ Projecton Source For Eclipse/src/controllers/ExtraDetailsChangeRequestController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import entities.MyFile;
 import assets.SqlResult;
+import assets.SqlAction;
 import assets.SqlFileAction;
 import assets.SqlQueryType;
 import boundries.ExtraDetailsChangeRequestBoundary;
@@ -22,20 +23,25 @@ public class ExtraDetailsChangeRequestController extends BasicController {
 		/* Create sql action */
 		ArrayList<Object> varArray = new ArrayList<Object>();
 		varArray.add(changeRequestID);
-		
-		SqlFileAction sqlFileAction = new SqlFileAction(SqlQueryType.DOWNLOAD_FILE_BY_CHANGE_REQUEST_ID, varArray);
+		SqlAction sqlFileAction = new SqlAction(SqlQueryType.DOWNLOAD_FILE_BY_CHANGE_REQUEST_ID, varArray);
+		//SqlFileAction sqlFileAction = new SqlFileAction(SqlQueryType.DOWNLOAD_FILE_BY_CHANGE_REQUEST_ID, varArray);
 		this.sendSqlActionToClient(sqlFileAction);
 	}
 	
-	
+	/*TODO: ask raviv : how myfile go inside sqlresult and its collapse the program */ 
 	@Override
 	public void getResultFromClient(SqlResult result) {
 		Platform.runLater(() -> {
 			switch(result.getActionType())
 			{
 				case DOWNLOAD_FILE_BY_CHANGE_REQUEST_ID:
+					MyFile downloadedFile = null;
 					this.unsubscribeFromClientDeliveries();
-					MyFile downloadedFile = (MyFile) result.getResultData().get(0).get(0);
+					if(!result.getResultData().isEmpty()) {
+						downloadedFile = new MyFile("R:\\ServerFiles"+result.getResultData().get(0).get(0)
+								+"."+result.getResultData().get(0).get(1));
+					}
+					//MyFile downloadedFile = (MyFile) result.getResultData().get(0).get(0);
 					myBoundary.displayFile(downloadedFile);
 				default:
 					break;
